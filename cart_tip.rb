d8605a2f9fa82b2bf2c9b684c3159160aa9f1bd7
luodaokai <1064569731@qq.com>
2016-04-16 23:26:17 +0800

    add code from lib

--- cart_tip.rb
+++ cart_tip.rb
@@ -66,3 +66,21 @@ end
 class OrdersController < ApplicationController
   skip_before_action :authorize, only: [:new,:create]
 end
+
+#config/application.rb
+#开启lib目录,让ｒａｉｌｓ加载ｌｉｂ中的代码.其中的代码(库代码)可以在MVC三个地方共享
+config.autoload_paths += %W(#{Rails.root}/lib)
+#如果一个库不能满足加载条件，可以使用ｒｅｑｕｉｒｅ机制。比如要加载lib/hello.rb文件，可以在任意MVC三个地方的文件开头，require "hello"。子目录下要加子目录的路径
+require "shipping/arimail"  # lib/shipping/arimail.rb
+
+
+Account.transaction do
+  ac = Account.where(id: id).lock("LOCK IN SHARE MODE").first
+  ac.balance -= amount if ac.balance > amount
+  ac.save
+end
+
+class Order < ActiveRecord::Base
+  scope :last_days, ->{ |days| where('updated< ?', days) }
+  scope :checks, where(pay_type: :check)
+end
