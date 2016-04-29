3935d62963b02cb01cb56ba87a7d52003dbf9747
luodaokai <guijihe@163.com>
2016-04-29 17:13:44 +0800

    object tree and work_with_ruby_threads

--- object_tree.rb
+++ object_tree.rb
@@ -3,7 +3,7 @@ class ObjectTree
   def initialize
     @sub_tasks = []
   end
-  
+
   def task
     total = 0
     @sub_tasks.each {|task| total += task.task}
---
+++ work_with_ruby_threads/file_uploader.rb
@@ -0,0 +1,32 @@
+require 'thread'
+
+class FileUpload
+  def initialize(files)
+    @files = files
+  end
+
+  def upload
+    threads = []
+    @files.each do |(filename, file_data)|
+      threads << Thread.new do
+        status = upload_to_s3(filename, file_data)
+        results << status
+      end
+
+      threads.each(&:join)
+    end
+
+    def results
+      @results ||= Queue.new
+    end
+
+    def upload_to_s3(filename, file)
+      # omitted
+    end
+  end
+end
+
+uploader = FileUploader.new('boots.png' => '*pretend png data*', 'shirts.png' => '*pretend png data*')
+
+uploader.upload
+puts uploader.upload
