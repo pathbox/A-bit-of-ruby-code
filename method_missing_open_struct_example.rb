7f2a365c4cfc3b60868346922512711218c5d11a
luodaokai <1064569731@qq.com>
2016-07-31 14:01:32 +0800

    method_missing for attributes build getter and setter

---
+++ method_missing_open_struct_example.rb
@@ -0,0 +1,22 @@
+class MyOpenStrruct
+  def initialize
+    @attributes = {}
+  end
+
+  def method_missing(name, *args)
+    puts args
+    puts name # method's name  getter and setter
+    attribute = name.to_s
+    if attribute =~ /=$/
+      @attributes[attribute.chop] = args[0]
+    else
+      @attributes[attribute]
+    end
+  end
+end
+
+puts MyOpenStrruct.new.attributes
+icecream = MyOpenStrruct.new
+icecream.flavor = "vanilla"
+puts "The flavor: #{icecream.flavor}"
+puts MyOpenStrruct.new.attributes
