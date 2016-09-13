be4974b63673d9888ff328461ed78d0f200a8750
luodaokai <1064569731@qq.com>
2016-09-13 21:37:38 +0800

    add to ActiveRecord Optimize

--- ActiveRecord_Optimize.rb
+++ ActiveRecord_Optimize.rb
@@ -16,8 +16,19 @@
 order by field 也会用到索引，其中的一个优化就是，对order by field　的field创建了联合索引，并且放在最后，速度也快了几十倍
 
 在某个查询想取第一个记录时, 使用take 或 limit(1)　代替　first　，速度会变快。
+email = current_company.customers.where("email like ?", "#{ip}\_%\_#{company_id}@temp.com").try(:first).try(:email)
+email = current_company.customers.where("email like ?", "#{ip}\_%\_#{company_id}@temp.com").take.try(:email)
+使用first　需要16s, 使用take　用了0.01s　来看这两个sql语句：
+SELECT `customers`.* FROM `customers` WHERE  `customers`.`company_id` = 13300 AND (email like "119.57.115.195_%_13300@temp.com") ORDER BY `customers`.`id` ASC LIMIT 1;
+SELECT `customers`.* FROM `customers` WHERE  `customers`.`company_id` = 13300 AND (email like "119.57.115.195_%_13300@temp.com") LIMIT 1;
+first其实用到了order by　id　所以,　没用到company_id,email的联合索引,使得在like查询上耗费了大量时间
 
 ３．　使用　explain　进行分析,　能给予你一个好的解决思路
 
 count(*)　是一种更好的选择。　当　count(*) from tables 效率最高。　count(*) from tables where 会进行全表的扫描,　所以最好给
 where 的字段建立索引
+
+４．另一种思路
+if　customers.pluck(:email).uniq.include? content
+if customers.where(email: content).plesent? 用这种方法直接查是否有这个邮件，只查row = 1 ，耗时 0.05s 以内，而且不耗内存，第一种方式产生的数组有可能消耗大量内存
+present? 方法还可以用　exists?代替，速度又会更快一点
