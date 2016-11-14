de6ecb5d36b0d0624d6c7073914dad3ba2302196
luodaokai <luodaokai@udesk.cn>
2016-11-14 11:51:16 +0800

    how to write DSL in ruby

---
+++ how_to_DSL_in_ruby.rb
@@ -0,0 +1,61 @@
+module Smokestack
+  @registry = {}
+
+  def self.registry
+    @registry
+  end
+
+  def self.define(&block)
+    definition_proxy = DefinitionProxy.new
+    definition_proxy.instance_eval(&block)
+  end
+
+  def self.build(factory_class, overrides = {})
+    instance = factory_class.new
+    factory = registry[factory_class]
+    attributes = factory.attributes.merge(overrides)
+    attributes.each do |attribute_name, value|
+      instance.send("#{attribute_name}=", value)
+    end
+    instance
+  end
+end
+
+class DefinitionProxy
+  def factory(factory_class, &block)
+    factory = Factory.new
+    factory.instance_eval(&block)
+    Smokestack.registry[factory_class] = factory
+  end
+end
+
+class Factory < BasicObject
+  def initialize
+    @attributes = {}
+  end
+
+  attr_reader :attributes
+
+  def method_missing(name, *args, &block)
+    @attributes[name] = args[0]
+  end
+end
+
+Smokestack.define do
+  factory User do
+    name "Gabe BW"
+    pet_name "Toto"
+  end
+end
+
+user = Smokestack.build(User)
+puts user.name
+#=> "Gabe BW"
+puts user.pet_name
+#=> "Toto"
+
+other_user = Smokestack.build(User, name: 'Bob')
+puts other_user.name
+#=> "Bob"
+puts other_user.pet_name
+#=> "Toto"
