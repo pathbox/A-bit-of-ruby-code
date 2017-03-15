bc5c2b3028c927a6932e8ef8839f570c10098264
luodaokai <1064569731@qq.com>
2017-03-15 18:18:00 +0800

    addtion to api logger for grape

--- aip_logger_for_grape.rb
+++ aip_logger_for_grape.rb
@@ -27,3 +27,24 @@ def response_log_data
       source_line: env['api.endpoint'].block.source_location[1]
   }
 end
+
+def request_log_data_2
+  # Escape the ampersand in the POST data.
+  rack_input = env["rack.input"].gets
+
+  if rack_input.present?
+    rack_input = rack_input.gsub("&","%26")
+    params_data = Rack::Utils.parse_query(rack_input, "&")
+  else
+    params_data = nil
+  end
+
+  request_data = {
+      method: env['REQUEST_METHOD'],
+      path:   env['PATH_INFO'],
+      query:  env['QUERY_STRING'],
+      params: params_data
+  }
+  # request_data[:user_id] = current_user.id if current_user
+  request_data
+end
