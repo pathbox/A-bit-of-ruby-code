8cac1ab6905d3a2bec569b089349a623dd7e3f69
luodaokai <1064569731@qq.com>
2017-03-28 11:49:09 +0800

    sidekiq source code

---
+++ sidekiq_mailer/source/sidekiq_mailer/lib/sidekiq_mailer.rb
@@ -0,0 +1,71 @@
+require 'sidekiq_mailer/version'
+require 'sidekiq_mailer/worker'
+require 'sidekiq_mailer/proxy'
+
+module Sidekiq
+  module Mailer
+    @@excluded_enviroments = nil
+
+    def self.excluded_enviroments=(envs)
+      @@excluded_enviroments = [*envs].map{|e| e && e.to_sym}
+    end
+
+    def self.excluded_enviroments
+      @@excluded_enviroments ||= []
+    end
+
+    def self.current_env
+      if defined?(Rails)
+        ::Rails.env
+      else
+        ENV['RAILS_ENV'].to_s
+      end
+    end
+
+    def self.excludes_current_environment?
+      !ActionMailer::Base.perform_deliveries || (excluded_enviroments && excluded_enviroments.include?(current_env.to_sym))
+    end
+
+    def self.included(base)
+      base.extend(ClassMethods)
+      base.class_attribute :sidekiq_options_hash
+    end
+
+    module ClassMethods
+       ##
+      # Allows customization for this type of Worker.
+      # Legal options:
+      #
+      #   :queue - use a named queue for this Worker, default 'mailer'
+      #   :retry - enable the RetryJobs middleware for this Worker, default *true*
+      #   :timeout - timeout the perform method after N seconds, default *nil*
+      #   :backtrace - whether to save any error backtrace in the retry payload to display in web UI,
+      #      can be true, false or an integer number of lines to save, default *false*
+
+      def sidekiq_options(opts={})
+        self.sidekiq_options_hash = get_sidekiq_options.merge(stringify_keys(opts || {}))
+      end
+
+      DEFAULT_OPTIONS = { 'retry' => true, 'queue' => 'mailer' }
+    end
+
+    def get_sidekiq_options
+      self.sidekiq_options_hash ||= DEFAULT_OPTIONS
+    end
+
+    def stringify_keys(hash)
+      hash.keys.each do |key|
+        hash[key.to_s] = hash.delete(key)
+      end
+      hash
+    end
+
+    def method_missing(method_name, *args)
+      if action_methods.include?(method_name.to_s)
+        Sidekiq::Mailer::Proxy.new(self, method_name, *args)
+      else
+        super
+      end
+    end
+  end
+end
---
+++ sidekiq_mailer/source/sidekiq_mailer/lib/sidekiq_mailer/proxy.rb
@@ -0,0 +1,38 @@
+class Sidekiq::Mailer::Proxy
+  delegate :to_s, :to => :actual_message
+
+  def initialize(mailer_class, method_name, *args)
+    @mailer_class = mailer_class
+    @method_name = method_name
+    *@args = *args
+  end
+
+  def actual_message
+    @actual_message ||= @mailer_class.send(:new, @method_name, *@args).message
+  end
+
+  def deliver
+    return deliver! if Sidekiq::Mailer.excludes_current_environment?
+    Sidekiq::Mailer::Worker.client_push(to_sidekiq)
+  end
+
+  def excluded_environment?
+    Sidekiq::Mailer.excludes_current_environment?
+  end
+
+  def deliver!
+    actual_message.deliver  # MyMailer.new().deliver
+  end
+
+  def method_missing(method_name, *args)
+    actual_message.send(method_name, *args)
+  end
+
+  def to_sidekiq
+    params = {
+      'class' => Sidekiq::Mailer::Worker,
+      'args' => [@mailer_class.to_s, @method_name, @args]
+    }
+    params.merge(@mailer_class.get_sidekiq_options)
+  end
+end
