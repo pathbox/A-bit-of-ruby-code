179ffbb9fe5e637c0deae22c5222e6df374ec837
luodaokai <luodaokai@udesk.cn>
2017-05-08 17:57:01 +0800

    yield 注释

--- yield_param.rb
+++ yield_param.rb
@@ -1,20 +1,20 @@
 def log_a(name)
   log = {title: "title"}
-  yield log
+  yield log # 将ｌｏｇ传到外部的block中
   log[:name] = name
   log
 end
 
 
 log_a("Cary") do |log|
-  puts log
+  puts log　# 从方法中　接收log
   log[:age] = 22
   log[:city] = "Beijin"
   puts log
 end
 
 log_a("Cary") do |txt|
-  puts txt
+  puts txt　　# 从方法中接收log
   txt[:age] = 22
   txt[:city] = "Beijin"
   puts txt
